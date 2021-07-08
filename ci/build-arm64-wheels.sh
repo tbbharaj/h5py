00e33f23c48cd8e246ec721c405f2347a81fa566
Tanveen Bharaj <tbbharaj@amazon.com>
2021-07-08 12:12:07 -0500

    travis build

--- ci/build-arm64-wheels.sh
+++ ci/build-arm64-wheels.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 set -e -u -x
 ls -lrt
+#docker run --rm -e PLAT=$PLAT -e PYTHON=$PYTHON -v $(realpath .):/src $DOCKER_IMAGE
+#bash /src/ci/get_hdf5_arm64.sh
 
-bash /src/ci/get_hdf5_arm64.sh
+docker run -it -v $(pwd):/src quay.io/pypa/manylinux2014_aarch64:latest
+cd /src/
+ls -lrt
+echo "Installing zlib with yum"
+export HDF5_VERSION=1.12.0
+export HDF5_DIR="/usr/local"
+yum -y install zlib-devel
+
+pushd /tmp
+ldconfig
+
+echo "Downloading & unpacking HDF5 ${HDF5_VERSION}"
+curl -fsSLO "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-${HDF5_VERSION%.*}/hdf5-$HDF5_VERSION/src/hdf5-$HDF5_VERSION.tar.gz"
+tar -xzvf hdf5-$HDF5_VERSION.tar.gz
+pushd hdf5-$HDF5_VERSION
+chmod u+x autogen.sh
+
+echo "Configuring, building & installing HDF5 ${HDF5_VERSION} to ${HDF5_DIR}"
+./configure --prefix $HDF5_DIR --enable-build-mode=production --with-szlib
+make -j $(nproc)
+make install
+popd
+
+echo "Cleaning up unnecessary files"
+#rm -r hdf5-$HDF5_VERSION
+#rm hdf5-$HDF5_VERSION.tar.gz
+
+#yum -y erase zlib-devel
+
+
 
 cd /src/
 ls -lrt
 
 # Create binary wheels
-/opt/python/${PYTHON}/bin/pip wheel /src/ -w wheelhouse/
+/opt/python/cp37-cp37m/bin/pip wheel /src/ -w wheelhouse/
+#/opt/python/${PYTHON}/bin/pip wheel /src/ -w wheelhouse/
 #python setup.py bdist_wheel
 
 ls -lrt wheelhouse/
